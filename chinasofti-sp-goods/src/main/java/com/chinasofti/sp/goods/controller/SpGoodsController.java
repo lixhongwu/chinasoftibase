166ec03e139792d55ca5446095734ea98e08ba66
Administrator <Administrator@H681PIH220VVPTF>
2017-09-30 16:32:05 +0800

    sp商品服务

---
+++ chinasofti-sp-goods/src/main/java/com/chinasofti/sp/goods/controller/SpGoodsController.java
@@ -0,0 +1,33 @@
+package com.chinasofti.sp.goods.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chinasofti.sp.goods.entity.SpGoodsinfo;
+import com.chinasofti.sp.goods.service.SpGoodsService;
+
+@RequestMapping("goods")
+@RestController
+public class SpGoodsController {
+	
+	@Autowired
+	SpGoodsService spGoodsService;
+	
+	/**
+	 * id查询
+	 * @param ids
+	 * @return
+	 */
+	@RequestMapping("/select/{ids}")
+	@ResponseBody
+	public SpGoodsinfo selectByPrimaryKey(@PathVariable String ids){
+		
+		return spGoodsService.selectByPrimaryKey(ids);
+		
+	}
+	
+	
+}
