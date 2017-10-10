b59443da93721299a10a62bab823741367ba2095
valuehusong <hasee@hasee-PC>
2017-10-10 17:03:28 +0800

    新增freemark继承

--- chinasofti-springcloud-demo/chinasofti-sp-consumer/src/main/java/com/chinasofti/springcloud/MicroserviceSimpleConsumerMovieApplication.java
+++ chinasofti-springcloud-demo/chinasofti-sp-consumer/src/main/java/com/chinasofti/springcloud/MicroserviceSimpleConsumerMovieApplication.java
@@ -1,13 +1,21 @@
 package com.chinasofti.springcloud;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import cn.org.rapid_framework.freemarker.directive.BlockDirective;
+import cn.org.rapid_framework.freemarker.directive.ExtendsDirective;
+import cn.org.rapid_framework.freemarker.directive.OverrideDirective;
+
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class MicroserviceSimpleConsumerMovieApplication {
 
   @Bean
@@ -18,4 +26,20 @@ public class MicroserviceSimpleConsumerMovieApplication {
   public static void main(String[] args) {
     SpringApplication.run(MicroserviceSimpleConsumerMovieApplication.class, args);
   }
+  
+  /*
+   * freemark模板页面继承
+   */
+  @Configuration
+	public class FreemarkerConfig {
+	    @Autowired
+	    freemarker.template.Configuration configuration;
+
+	    @PostConstruct
+	    public void setSharedVariable(){
+	        configuration.setSharedVariable("block", new BlockDirective());
+	        configuration.setSharedVariable("override", new OverrideDirective());
+	        configuration.setSharedVariable("extends", new ExtendsDirective());
+	    }
+	}
 }
