784b5d8f85ba81829553ace39a0c61e408c5da23
KanMeng <753162364@qq.com>
2017-10-12 09:34:19 +0800

    zull配置修改

--- chinasofti-springcloud-demo/chinasofti-sp-consumer/src/main/java/com/chinasofti/springcloud/controller/UserController.java
+++ chinasofti-springcloud-demo/chinasofti-sp-consumer/src/main/java/com/chinasofti/springcloud/controller/UserController.java
@@ -22,8 +22,8 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 /**
- * user控制器
- * 访问方式启动本地eureka服务器和服务提供者，通过ip+port加上/user/id
+ * user控制器 访问方式启动本地eureka服务器和服务提供者，通过ip+port加上/user/id
+ * 
  * @author husong
  *
  */
@@ -33,39 +33,42 @@ public class UserController {
 
 	@Value("${user.userServicePath}")
 	private String userServicePath;
-	
-	@HystrixCommand(fallbackMethod="findByIdFallback")//熔断
+
+	@HystrixCommand(fallbackMethod = "findByIdFallback") // 熔断
 	@GetMapping("/user/{id}")
 	public User findById(@PathVariable Long id) {
 
 		return this.restTemplate.getForObject(this.userServicePath + "spuser/" + id, User.class);
 	}
-    @RequestMapping("/user/web")
+
+	@RequestMapping("/user/web")
 	public ModelAndView getview() {
-		return  new ModelAndView("children");
+		return new ModelAndView("children");
 	}
-    
-    @PostMapping("/user/save")
+
+	@PostMapping("/user/save")
 	public ResponseEntity<String> save(User user) {
 		HttpHeaders headers = new HttpHeaders();
 		String url = "http://localhost:7801/spuser/save";
-		//  请勿轻易改变此提交方式，大部分的情况下，提交方式都是表单提交
+		// 请勿轻易改变此提交方式，大部分的情况下，提交方式都是表单提交
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		//  封装参数，千万不要替换为Map与HashMap，否则参数无法传递
-		MultiValueMap<String, String> params= new LinkedMultiValueMap<String, String>();
-		//  将对象转为json传送出去,服务端再转换回来, 也支持中文
+		// 封装参数，千万不要替换为Map与HashMap，否则参数无法传递
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+		// 将对象转为json传送出去,服务端再转换回来, 也支持中文
 		Gson gson = new Gson();
 		String json = gson.toJson(user);
 		params.add("user", json);
-		HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(params, headers);
-		//  执行HTTP请求
+		HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(params,
+				headers);
+		// 执行HTTP请求
 		ResponseEntity<String> response = restTemplate.postForEntity(url, requestEntity, String.class);
 		return response;
 	}
-    //findById发生熔断返回方法
-    public User findByIdFallback(Long id) {
+
+	// findById发生熔断返回方法
+	public User findByIdFallback(Long id) {
 		return new User();
-    	
-    }
-    
+
+	}
+
 }
