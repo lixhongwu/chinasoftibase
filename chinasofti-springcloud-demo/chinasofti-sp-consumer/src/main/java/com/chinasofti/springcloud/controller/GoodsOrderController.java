f31730ba0ae4cda54bc5fda5ebf44476b2df1b62
misterNoah <123545052@qq.com>
2017-10-26 11:30:32 +0800

    订单配置文件转到配置中心

--- chinasofti-springcloud-demo/chinasofti-sp-consumer/src/main/java/com/chinasofti/springcloud/controller/GoodsOrderController.java
+++ chinasofti-springcloud-demo/chinasofti-sp-consumer/src/main/java/com/chinasofti/springcloud/controller/GoodsOrderController.java
@@ -1,6 +1,5 @@
 package com.chinasofti.springcloud.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
