417d78b93c8944e2ff73a4a55c4ae016e581bb9e
hjl <huangjinlin@chinasofti.com>
2017-11-02 12:59:01 +0800

    改名

--- chinasofti-wxinterface/src/main/java/com/huateng/weixin/WXInterfaceApplication.java
+++ chinasofti-wxinterface/src/main/java/com/huateng/weixin/WXInterfaceApplication.java
@@ -8,10 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class Application
+public class WXInterfaceApplication
 {
     public static void main( String[] args )
     {
-    	SpringApplication.run(Application.class,args);
+    	SpringApplication.run(WXInterfaceApplication.class,args);
     }
 }
